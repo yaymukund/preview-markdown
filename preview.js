e5b381604932963904a1cbcc40fbe17221d7e151
Russ Edwards <russell.w.edwards@gmail.com>
2017-10-05 19:37:28 -0400

    Update away from timed generation

--- preview.js
+++ preview.js
@@ -2,7 +2,8 @@ var marked = require('marked'),
     hljs = require('highlight.js'),
     fs = require('fs'),
     path = process.argv[2],
-    TEMPLATE = fs.readFileSync('./template.html', 'utf-8');
+    TEMPLATE = fs.readFileSync('./template.html', 'utf-8'),
+    fsTimeout = false;
 
 marked.setOptions({
   gfm: true,
@@ -11,19 +12,23 @@ marked.setOptions({
   }
 });
 
-var createMarkdownFile = function(path) {
-  fs.readFile(path, 'utf-8', function(err, data) {
-    if (err) throw err;
-
-    var html = TEMPLATE.replace('CONTENT', marked(data)),
-        htmlFilename = path.replace('^.*[\\\/]', '')
-                           .replace(/\.(md|markdown)$/i, '.html');
-
-    fs.writeFile(htmlFilename, html, function(err) {
+fs.watch(path, (eventType, filename) => {
+  if (!fsTimeout) {
+    fsTimeout = true;
+    console.log(`File event: ${eventType}.`);
+    fs.readFile(path, 'utf-8', function(err, data) {
       if (err) throw err;
-      console.log('Created ' + htmlFilename);
+  
+      var html = TEMPLATE.replace('CONTENT', marked(data)),
+          htmlFilename = path.replace('^.*[\\\/]', '')
+                             .replace(/\.(md|markdown)$/i, '.html');
+  
+      fs.writeFile(htmlFilename, html, function(err) {
+        if (err) throw err;
+        console.log(`Created ${htmlFilename}`);
+      });
     });
-  });
-};
-
-setInterval(function() { createMarkdownFile(path); }, 2000);
+    //debounce for Windows file changes
+    fsTimeout = setTimeout(() => { fsTimeout = false }, 250);
+  }
+});
